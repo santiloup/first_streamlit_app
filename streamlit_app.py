9449a5bacb872379a58b9493f20903210f7f215b
santiloup <65695108+santiloup@users.noreply.github.com>
2022-06-22 15:08:52 +0200

    Added ingredient list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
+import pandas
 import streamlit
 
+# Main menu
 streamlit.title("My Mom's new healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# ingredient list
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataset(my_fruit_list)
