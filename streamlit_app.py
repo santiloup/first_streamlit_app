f32783d779080c55f0346fbd112d07efb4ef180c
santiloup <65695108+santiloup@users.noreply.github.com>
2022-06-29 12:29:45 +0200

    fruityvice response for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list.loc[fruit_selection])
 
 # incorporate frutyvice request in streamlit
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json)
