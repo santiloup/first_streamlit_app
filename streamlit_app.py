74544994e90f499055ce4a4a9910350687a6677f
santiloup <65695108+santiloup@users.noreply.github.com>
2022-07-12 11:29:18 +0200

    syntax line 43

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Adding a new fruit from fruityvice menu into snowflake list
-new_fruit = streamlit.select("What fruit would you like to add to the fruit load list?", list(my_fruit_list.Fruit))
+new_fruit = streamlit.selectbox("What fruit would you like to add to the fruit load list?", list(my_fruit_list.Fruit))
 streamlit.write('New fruit selected: ', new_fruit)
 fruit_add_query = "insert into pc_rivery_db.public.fruit_load_list values (" & new_fruit & ")"
 streamlit.write(fruit_add_query)
